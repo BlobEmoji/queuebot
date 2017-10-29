fb38f08b3a7d52130cdca0ed127fd0f400f02f06
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-10-29 13:16:07 -0700

    logging: log to stdout as well

--- run.py
+++ run.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 import logging
 
+import sys
+
 import config
 from queuebot.bot import Queuebot
 
 logging.getLogger('discord').setLevel(logging.INFO)
 logging.getLogger('queuebot').setLevel(logging.DEBUG)
 
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
 handler = logging.FileHandler(filename='queuebot.log', encoding='utf-8', mode='a')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler.setFormatter(formatter)
+stream = logging.StreamHandler(stream=sys.stdout)
+stream.setFormatter(formatter)
+
 logging.getLogger().addHandler(handler)  # Log everything to queuebot.log.
+logging.getLogger().addHandler(stream)   # Log everything to stdout.
 
 bot = Queuebot(command_prefix='q!')
 bot.discover_exts('queuebot/cogs')
