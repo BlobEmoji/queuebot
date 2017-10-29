128c988737d005d8da2ac80830f6954a1896dc8f
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-10-29 14:26:24 -0700

    discovery: automatically load cogs

--- queuebot/bot.py
+++ queuebot/bot.py
@@ -1,7 +1,11 @@
+import importlib
+import inspect
 import logging
 from pathlib import Path
 
 from discord.ext import commands
+
+from queuebot.cog import Cog
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +14,7 @@ class Queuebot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Not really needed.
+        # Remove default help command.
         self.remove_command('help')
 
     def discover_exts(self, directory: str):
@@ -25,4 +29,18 @@ class Queuebot(commands.Bot):
         logger.info('Loading extensions: %s', exts)
 
         for ext in exts:
-            self.load_extension('queuebot.cogs.' + ext)
+            name = 'queuebot.cogs.' + ext
+            module = importlib.import_module(name)
+
+            cogs = inspect.getmembers(
+                module, predicate=lambda obj: inspect.isclass(obj) and issubclass(obj, Cog) and obj is not Cog
+            )
+
+            for name, cog in cogs:
+                logger.info('Automatically adding cog: %s', cog.__name__)
+                self.add_cog(cog(self))
+
+            if hasattr(module, 'setup'):
+                module.setup(self)
+
+            self.extensions[name] = module
