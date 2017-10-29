a8e7c94e55c4686958eae372f5bdf65cffce4c3b
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-10-29 14:27:21 -0700

    utilities: convert to autoload, add q!roles, q!help

--- queuebot/cogs/utilities.py
+++ queuebot/cogs/utilities.py
@@ -1,19 +1,70 @@
-from discord.ext.commands import Context, command
+import inspect
 
-from queuebot.utils import Timer
-from ..bot import Queuebot
+from discord.ext.commands import Context, command, Paginator, Group
+
+from queuebot.cog import Cog
+from queuebot.utils import Timer, Table
 
 
-class Utilities:
-    def __init__(self, bot: Queuebot):
-        self.bot = bot
+class Utilities(Cog):
+    @command()
+    async def help(self, ctx: Context):
+        """Shows this help message."""
+
+        # Use a Paginator, so if we go over the 2,000 character limit, we will send multiple messages.
+        paginator = Paginator(prefix='', suffix='')
+
+        # Header
+        paginator.add_line(f'**{ctx.bot.user.name} Command Reference:**')
+        paginator.add_line()
+
+        # `Group`s are `Command`s, so we cannot use `Command` in our `isinstance` checks here.
+        regular_commands = [c for c in ctx.bot.commands if not isinstance(c, Group)]
+        groups = [c for c in ctx.bot.commands if isinstance(c, Group)]
+
+        format_cmd = lambda c: f'`{ctx.prefix}{c.signature}`: {c.help}'
+
+        for cmd in regular_commands:
+            paginator.add_line(format_cmd(cmd))
+
+        for group in groups:
+            paginator.add_line()
+
+            # The group's help message acts as a category header. Your command group docstrings should be
+            # like """Infractions""" or """Queue Management""", as they will be displayed here.
+            paginator.add_line(f'**{group.help}**')
+            paginator.add_line()
+
+            if len(inspect.signature(group.callback).parameters) > 2:
+                # The @group() command has arguments other than self and ctx, it probably does something. Add it here.
+                # Keep in mind that command groups are technically commands.
+                paginator.add_line(format_cmd(group))
+
+            # Add all subcommands.
+            for cmd in group.commands:
+                paginator.add_line(format_cmd(cmd))
+
+            paginator.add_line()
+
+        for page in paginator.pages:
+            await ctx.send(page)
 
     @command()
     async def ping(self, ctx: Context):
+        """Makes the bot respond with \"Pong!\""""
         with Timer() as timer:
             msg = await ctx.send('Po\N{EM DASH}')
         await msg.edit(content=f'Pong! {timer}')
 
+    @command()
+    async def roles(self, ctx: Context):
+        """Views roles in this server."""
+        table = Table('ID', 'Name', 'Colour')
 
-def setup(bot: Queuebot):
-    bot.add_cog(Utilities(bot))
+        roles = sorted(ctx.guild.roles, key=lambda r: r.position, reverse=True)
+
+        for role in roles:
+            table.add_row(str(role.id), role.name, str(role.color))
+
+        rendered = await table.render(ctx.bot.loop)
+        await ctx.send('```\n' + rendered + '\n```')
