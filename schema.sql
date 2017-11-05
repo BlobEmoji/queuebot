2ef12f2fa5edc895a54f9d687dd3440b8682d9bd
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-11-04 17:07:55 -0700

    add comments to schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,21 @@
 create table if not exists suggestions (
+    -- emoji id
     idx serial primary key,
+
+    -- user that submitted the emoji
     user_id bigint,
+
+    -- message id in the #council-queue
     council_message_id bigint,
+
+    -- message id in the #approval-queue
     public_message_id bigint,
+
+    -- emoji data
     emoji_id bigint,
     emoji_name text,
+
+    -- votes
     upvotes int default 0,
     downvotes int default 0
 );
