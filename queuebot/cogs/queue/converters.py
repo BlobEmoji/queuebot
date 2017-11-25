322f29412de340e5cb28ca3e38804535c534dab4
Gorialis <sansgorialis@gmail.com>
2017-11-26 07:08:54 +0900

    Introduce PartialSuggestionConverter (fixes #17)

--- queuebot/cogs/queue/converters.py
+++ queuebot/cogs/queue/converters.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from discord.ext import commands
 
 from queuebot.cogs.queue.suggestion import Suggestion
@@ -12,3 +14,17 @@ class SuggestionConverter(commands.Converter):
             raise commands.BadArgument('Invalid ID.')
         except Suggestion.NotFound:
             raise commands.BadArgument('Suggestion not found.')
+
+class PartialSuggestionConverter(commands.Converter):
+    async def convert(self, ctx: commands.Context, argument: str):
+        try:
+            sugg_id = int(argument)
+            suggestion = await Suggestion.get_from_id(sugg_id)
+            return (suggestion.record["idx"], suggestion.emoji_url)
+        except (ValueError, Suggestion.NotFound):
+            if argument.startswith(("http://", "https://")):
+                await asyncio.sleep(1)  # wait up to a second for the embed to load in
+                if ctx.message.embeds and ctx.message.embeds[0].image:
+                    return (None, ctx.message.embeds[0].image.proxy_url)
+            
+            raise commands.BadArgument("Couldn't resolve to suggestion or image.")
